fa7b57a66bf04e46784c2a2096ced6505618f39d
Nicole Steadman <nsteadman@gmail.com>
2021-11-04 09:04:33 -0700

    commit tree cbb1703d90b7c444fdefa9507cde032471808f4a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadImages();
+    loadBreedOptions();
+  });
+  
+  function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+      .then(response => response.json())
+      .then(results => {
+        results.message.forEach(image => addImage(image))
+      });
+  }
+  
+  function addImage(dogPic) {
+    let container = document.querySelector('#dog-image-container');
+    let newImageEl = document.createElement('img');
+    newImageEl.src = dogPic;
+    container.appendChild(newImageEl);
+  }
+
+  function loadBreedOptions() {
+      const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+      fetch(breedUrl)
+      .then(response => response.json())
+      .then(results => {
+          breeds = Object.keys(results.message);
+          addBreedList(breeds);
+
+      });
+  }
+
+  function addBreedList(breeds) {
+    let breedUl = document.getElementById("dog-breeds");
+    removeChildren(breedUl);
+    breeds.forEach(breed => addBreed(breed));
+  }
+
+  function removeChildren(element) {
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+  }
+  
+  function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+  }
+  
+  function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function (event) {
+      selectBreedsStartingWith(event.target.value);
+    });
+  }
+  
+  function addBreed(breed) {
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    li.innerText = breed;
+    li.style.cursor = 'pointer';
+    ul.appendChild(li);
+    li.addEventListener('click', updateColor);
+  }
+  
+  function updateColor(event) {
+    event.target.style.color = 'palevioletred';
+  }
